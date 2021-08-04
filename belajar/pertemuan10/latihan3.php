7a48ec1aaec3fa7a95befffbcd360d82ada02809
Roger Alexsander Siburian <zrogeru9@gmail.com>
2021-08-04 20:25:29 +0700

    menambahkan fitur insert data

--- belajar/pertemuan10/latihan3.php
+++ belajar/pertemuan10/latihan3.php
@@ -18,6 +18,9 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
 <body>
   <h3>Daftar Mahasiswa</h3>
 
+  <a href="tambah.php">Tambah Daftar Mahasiswa</a>
+  <br><br>
+
   <table border="1" cellpadding="10" cellspacing="0">
     
     <tr>
